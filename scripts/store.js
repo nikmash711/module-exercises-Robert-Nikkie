30a43cf61ebff838fa92d40d363a7eff364e40f3
RP Boyle <rpboyle11@yahoo.com>
2018-11-05 15:17:34 -0500

    added new functions to store.js that will eventually replace functions in shopping-list.js

--- scripts/store.js
+++ scripts/store.js
@@ -26,5 +26,28 @@ const store = (function () {
     }
   }
 
-  return {items, hideCheckedItems, searchTerm};
+  function findAndToggleChecked(id) {
+    //this.findById(id).checked = !this.findById(id).checked;
+    let checkedId = this.findById(id).checked;
+    checkedId = !checkedId;
+
+  } 
+  
+  function findAndUpdateName(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch(error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  }
+
+  function findAndDelete(id) {
+    //this.items = this.items.filter(item => item.id !== id);
+
+    let index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index, 1);
+  }
+
+  return {items, hideCheckedItems, searchTerm, findAndDelete, findAndUpdateName, findAndToggleChecked, findById, addItem};
 }() );
