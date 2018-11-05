40b6f47dc971f669671147373fd0508587c2d2ab
RP Boyle <rpboyle11@yahoo.com>
2018-11-05 12:34:04 -0500

    testing name function

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,6 +1,21 @@
 'use strict';
+/* global cuid */
 
 const Item = (function(){
-  const foo = 'bar';
-  return {};
+  function validateName(name) {
+    //this validates that name is true...come back to this if error
+    if(!name) {
+      throw new Error('Name does not exist');
+    }
+  }
+  
+  function create(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false,
+    }
+  }
+
+  return {validateName, create};
 }()); 
